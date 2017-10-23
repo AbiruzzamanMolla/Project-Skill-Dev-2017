223bd89b8dd37385b1085511f7595a220c2248f4
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2017-10-24 00:38:50 +0600

    updating files

---
+++ inc/footer.php
@@ -0,0 +1,16 @@
+    <footer class="container">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2017 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="style/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="style/assets/js/vendor/popper.min.js"></script>
+    <script src="style/dist/js/bootstrap.min.js"></script>
+    <script src="style/dist/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="offcanvas.js"></script>
+  </body>
+</html>
+<?php mysqli_close($connection); ?> 
---
+++ inc/header.php
@@ -0,0 +1,57 @@
+<?php include 'db.php'; ?> 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="style/favicon.ico">
+
+    <title>Our website</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="style/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style/carousel.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="offcanvas.css" rel="stylesheet">
+    <script src="/style/dist/js/bootstrap.min.js"></script>
+    <script src="/style/dist/js/bootstrap.js"></script>
+    <style>
+        .navbar-custom {
+    background-color: #ff5500;
+}
+
+/* change the brand and text color */
+.navbar-custom .navbar-brand,
+.navbar-custom .navbar-text {
+    color: rgba(255,255,255,.8);
+}
+
+/* change the link color */
+.navbar-custom .navbar-nav .nav-link {
+    color: rgba(255,255,255,.5);
+}
+
+/* change the color of active or hovered links */
+.navbar-custom .nav-item.active .nav-link,
+.navbar-custom .nav-item:hover .nav-link {
+    color: #ffffff;
+}
+
+/* for dropdown only - change the color of droodown */
+.navbar-custom .dropdown-menu {
+    background-color: #ff5500;
+}
+.navbar-custom .dropdown-item {
+    color: #ffffff;
+}
+.navbar-custom .dropdown-item:hover,
+.navbar-custom .dropdown-item:focus {
+    color: #333333;
+    background-color: rgba(255,255,255,.5);
+}
+    </style>
+  </head>
+    <body>
