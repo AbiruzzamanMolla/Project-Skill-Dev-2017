f30f3e2dce9084a6fd939043cae6b94e133d5c77
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2017-10-24 15:16:35 +0600

    adding new files

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md fixed-top navbar-custom">
-  <a class="navbar-brand" href="#">HumanDebuts</a>
+  <a class="navbar-brand" href="./index.php">মানবতার আত্নপ্রকাশ</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,62 +7,27 @@
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">হোমপেইজ
+         <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Poor Students</a>
+        <a class="nav-link" href="poor_students.php">গরীব শিক্ষার্থী
+        </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Poor Rugi</a>
+        <a class="nav-link disabled" href="helpless.php">অসহায় রোগী</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Founds</a>
+        <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ফান্ড</a>
         <div class="dropdown-menu" aria-labelledby="dropdown01">
-          <a class="dropdown-item" href="#">Atimkhan</a>
-          <a class="dropdown-item" href="#">Briddasrom</a>
+          <a class="dropdown-item" href="#">এতিমখানা</a>
+          <a class="dropdown-item" href="#">বৃদ্ধাশ্রম</a>
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Notice</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Registation</a>
+        <a class="nav-link" href="#">বিজ্ঞপ্তি</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+      <a class="text-right pull-right" href="reg.php"><button class="pull-right form-inline my-2 my-lg-0 btn btn-outline-success my-2 my-sm-0" type="submit">আবেদন করুন</button></a>
   </div>
 </nav>
-
-<!-- <nav class="navbar navbar-expand-sm navbar-custom">
-    <a href="/" class="navbar-brand">Custom</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCustom">
-        <i class="fa fa-bars fa-lg py-1 text-white"></i>
-    </button>
-    <div class="navbar-collapse collapse" id="navbarCustom">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-        </ul>
-        <span class="ml-auto navbar-text">Text</span>
-    </div>
-</nav> -->
--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,14 +1,17 @@
    <div class="col-6 col-md-3 sidebar-offcanvas" id="sidebar">
-          <div class="list-group">
-            <a href="#" class="list-group-item active">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-          </div>
+          <div class="well">
+			<h4>সাম্প্রতিক বিজ্ঞপ্তি</h4>
+			<div class="row">
+				<div class="col-lg-6">
+					<ul class="list-unstyled">
+          <li>dfdsafdsf</li>
+					<li>dfdsafdsf</li>
+					<li>dfdsafdsf</li>
+
+					</ul>
+				</div>
+				<!-- /.col-lg-6 -->
+			</div>
+			<!-- /.row -->
+		</div>
         </div><!--/span-->
